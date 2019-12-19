d72fdec210f8c2e962b283cb205f0dbbff814908
soulintzis <esoulintzhs@gmail.com>
2019-12-19 21:30:31 +0200

    Update generateRandomIpAdresses.py

--- generateRandomIpAdresses.py
+++ generateRandomIpAdresses.py
@@ -8,26 +8,24 @@ output = input("Name the output file(without extension): ")
 
 counter = 0
 while counter < number_of_ip_addresses:
-    number_of_class_A_ips = randint(0,255)
-    while number_of_class_A_ips > number_of_ip_addresses:
-        number_of_class_A_ips = randint(0,255)
     first_part = randint(0,255)
-    print('A: ' + str(number_of_class_A_ips) + '-' + str(first_part))
-    for x in range(number_of_class_A_ips):
-
-        number_of_class_B_ips = randint(0,255)
-        while number_of_class_B_ips > number_of_ip_addresses+number_of_class_A_ips:
-            number_of_class_A_ips = randint(0,255)
-
+    # print('A: ' + str(number_of_class_A_ips) + '-' + str(first_part))
+    for x in range(randint(0,15)):
         second_part = randint(0,255)
-        print('B: ' + str(number_of_class_B_ips) + '-' + str(second_part))
-        for y in range(number_of_class_B_ips):
+        # print('B: ' + str(number_of_class_B_ips) + '-' + str(second_part))
+        for y in range(randint(0,25)):
             third_part = randint(0,255)
-            forth_part = randint(0,255)
-            ip = str(first_part) + '.' + str(second_part) + '.' + str(third_part) + '.' + str(forth_part)
-            ips.append(ip)
-            counter = counter + 1
+            # print('C: ' + str(number_of_class_C_ips) + '-' + str(third_part))
+            for z in range(randint(0,35)):
+                forth_part = randint(0,255)
+                ip = str(first_part) + '.' + str(second_part) + '.' + str(third_part) + '.' + str(forth_part)
+                ips.append(ip)
+                if counter >= number_of_ip_addresses:
+                    break
+                counter += 1
 
 with open(output  + '.txt', 'w') as f:
     for items in ips:
         f.write("%s\n" % items)
+
+print("The process has completed successfully")
