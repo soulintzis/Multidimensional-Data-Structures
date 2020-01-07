65feff0169e69fc6f94327730aadf596b98ee72d
soulintzis <esoulintzhs@gmail.com>
2020-01-07 18:03:30 +0200

    bloom_filters

--- BloomFilters/bloomfilters.py
+++ BloomFilters/bloomfilters.py
@@ -1,5 +1,7 @@
 import bitarray 
 from random import shuffle
+import os.path
+from os import path
 import hashlib 
 import murmurhash
 import array
@@ -13,17 +15,23 @@ imported_data = list()
 #We use sha256 for the best uniformity, but it is quite costly
 hash_functions = [hashlib.sha256]
 
-# bit_array = bitarray(int(size_of_filter))
-# bit_array.setall(0)
-
-def importData(input_filename):
-    with open(input_filename, 'r') as f:
-        return f.read().split('\n')
+def importData():
+    while True:
+        file_name = input('Please type in the path to your file and press "Enter":')
+        try:
+            with open(file_name, 'r') as f:
+                return f.read().split('\n')
+            f.close()
+        except FileNotFoundError:
+            print('Wrong file or file path')
+        else:
+            break
 
 def exportData(output_filename, output_data):
     with open(output_filename, 'w') as f:
         for items in output_data:
             f.write("%s\n" %items)
+    f.close()
     print('The data exported successfully')
 
 # Print iterations progress
@@ -54,11 +62,12 @@ class BloomFilter:
             self.number_of_items = number_of_items
             self.number_of_bits = number_of_bits
             self.number_of_hf = number_of_hf
-            self.prop_false_positive =  self.calc_prop_false_positive
+            self.calc_prop_false_positive()
         elif prop_false_positive is not None:
+            self.prop_false_positive = prop_false_positive
             self.number_of_items = number_of_items
-            self.number_of_bits = self.calc_number_of_bits
-            self.number_of_hf = self.calc_number_of_hf
+            self.calc_number_of_bits()
+            self.calc_number_of_hf()
         else:
             self.number_of_items = number_of_items
             self.prop_false_positive = prop_false_positive
@@ -83,8 +92,8 @@ class BloomFilter:
                 return False
         return True
 
-    def create_bit_array(self, number_of_bits):
-        self.bit_array = bitarray.bitarray(int(number_of_bits))
+    def create_bit_array(self):
+        self.bit_array = bitarray.bitarray(int(self.number_of_bits))
         self.bit_array.setall(0)
 
     def exportBitArray(self, name):
@@ -93,12 +102,18 @@ class BloomFilter:
             self.bit_array.tofile(f)
         f.close()
 
-    def import_bit_array(self, name):
-        self.name = name + '.bin'
+    def import_bit_array(self, file_name):
         self.bit_array = bitarray.bitarray()
-        with open('bit_array.bin', 'rb') as f:
-            self.bit_array.fromfile(f)
-        f.close()
+        while True:
+            try:
+                with open(file_name, 'rb') as f:
+                    self.bit_array.fromfile(f)
+                f.close()
+            except FileNotFoundError:
+                print("Wrong file or file path")
+                file_name = input('Please type in the path to your binary file and press "Enter":')
+            else:
+                break
         self.number_of_bits = len(self.bit_array)
         self.calc_prop_false_positive()
 
@@ -115,13 +130,58 @@ class BloomFilter:
 print('---------------------------------------------------------------------------------------------')
 exists_bit_array = input('Do you want to import an already existed Bit Array?(y/n)')
 if exists_bit_array == 'y' or exists_bit_array == 'Y':
-    file_name = input('Give the name of the file(without extension):') 
-    items = input('Give the number of items you initially intended to import to the BF:')
-    num_of_hf = input('Give the number of hash functions you initially intended to use:')
-    new_filter = BloomFilter(None, int(items), None, int(num_of_hf))
+    file_name = input('Please type in the path to your binary file and press "Enter":')
+    num_items = input('Give the number of items you initially intended to import to the BF:')
+    num_of_hf = input('How many times you want to hash each item?')
+    new_filter = BloomFilter(None, int(num_items), None, int(num_of_hf))
     BloomFilter.import_bit_array(new_filter, file_name)
+else:
+    dataset = importData()
+    num_items = input('How many items do you want to insert on the Bloom Filter?')
+    if len(dataset) > int(num_items):
+        print('The dataset that you imported is greater than the number of items you have declare that the BF possibly can handle.')
+        num_items = input('Please enter the number of items again(take into consideration that the file \n that you imported containes {} items): '.format(len(dataset)))
+    print('1-- Do you want to give the Probability of false positives and take the optimal number of hash functions and size of bit array?')
+    print('2-- Do you want to insert custom values for the Bloom Filter?')
+    optim_values = None
+    while optim_values is None:
+        optim_values = input('Choose one from the above:')
+        if optim_values == '1':
+            optim_values = True
+        elif optim_values == '2':
+            optim_values = False
+        else:
+            optim_values = None
+            print('Please try again!')
 
-# num_items = input('How many items do you want to insert on the Bloom Filter?')
+    if optim_values:
+        prop_fp = input('Give the Probability of false positives(fraction between 0 and 1):')
+        new_filter = BloomFilter(float(prop_fp), int(num_items), None, None)
+        new_filter.create_bit_array()
+        print('Number of items: {}'.format(new_filter.number_of_items))
+        print('Bit array size: {}'.format(new_filter.number_of_bits))
+        print('Number of hash functions: {}'.format(new_filter.number_of_hf))
+        print('Probability of false positives: {}'.format(new_filter.prop_false_positive))
+    else:
+        size_bit_array = input('Give the the size of the bit array:')
+        num_of_hf = input('How many times you want to hash each item?')
+        new_filter = BloomFilter(None, int(num_items), int(size_bit_array), int(num_of_hf))
+        print('Number of items: {}'.format(new_filter.number_of_items))
+        print('Bit array size: {}'.format(new_filter.number_of_bits))
+        print('Number of hash functions: {}'.format(new_filter.number_of_hf))
+        print('Probability of false positives: {}'.format(new_filter.prop_false_positive))
+            
+    # Initial call to print 0% progress
+    printProgressBar(0, len(dataset), prefix = 'Progress:', suffix = 'Complete', length = 50)
+    add_start = time.time()
+    for i, item in enumerate(dataset):
+        new_filter.add_item_to_filter(item, hash_functions[0])
+        # Update Progress Bar
+        printProgressBar(i + 1, len(dataset), prefix = 'Progress:', suffix = 'Complete', length = 50)
+    add_end = time.time()
+
+    print('Total time: {}s'.format(add_end - add_start))
+
 
 # print('1-- Give the size of the BF and the Probability of false positives(fraction between 0 and 1).')
 # print('2-- Give the size of the BF, the ')
