39f2d42e56484597babd9b6b274d07e1b89bae23
soulintzis <esoulintzhs@gmail.com>
2020-01-07 20:41:12 +0200

    improvements

--- BloomFilters/bloomfilters.py
+++ BloomFilters/bloomfilters.py
@@ -1,13 +1,11 @@
-import bitarray 
 from random import shuffle
-import os.path
-from os import path
-import hashlib 
 import murmurhash
+import bitarray 
+import hashlib 
 import array
+import hmac
 import math
 import time
-import hmac
 
 results = list()
 imported_data = list()
@@ -15,15 +13,15 @@ imported_data = list()
 #We use sha256 for the best uniformity, but it is quite costly
 hash_functions = [hashlib.sha256]
 
-def importData():
+def importData(file_name):
     while True:
-        file_name = input('Please type in the path to your file and press "Enter":')
         try:
             with open(file_name, 'r') as f:
                 return f.read().split('\n')
             f.close()
         except FileNotFoundError:
             print('Wrong file or file path')
+            file_name = input('Please type in the path to your file and press "Enter":')
         else:
             break
 
@@ -133,16 +131,19 @@ if exists_bit_array == 'y' or exists_bit_array == 'Y':
     file_name = input('Please type in the path to your binary file and press "Enter":')
     num_items = input('Give the number of items you initially intended to import to the BF:')
     num_of_hf = input('How many times you want to hash each item?')
+    print()
     new_filter = BloomFilter(None, int(num_items), None, int(num_of_hf))
     BloomFilter.import_bit_array(new_filter, file_name)
 else:
-    dataset = importData()
+    file_name = input('Please type in the path to your file and press "Enter":')
+    dataset = importData(file_name)
     num_items = input('How many items do you want to insert on the Bloom Filter?')
     if len(dataset) > int(num_items):
         print('The dataset that you imported is greater than the number of items you have declare that the BF possibly can handle.')
         num_items = input('Please enter the number of items again(take into consideration that the file \n that you imported containes {} items): '.format(len(dataset)))
-    print('1-- Do you want to give the Probability of false positives and take the optimal number of hash functions and size of bit array?')
+    print('\n1-- Do you want to give the Probability of false positives and take the optimal number of hash functions and size of bit array?')
     print('2-- Do you want to insert custom values for the Bloom Filter?')
+    print()
     optim_values = None
     while optim_values is None:
         optim_values = input('Choose one from the above:')
@@ -156,82 +157,106 @@ else:
 
     if optim_values:
         prop_fp = input('Give the Probability of false positives(fraction between 0 and 1):')
+        print()
         new_filter = BloomFilter(float(prop_fp), int(num_items), None, None)
         new_filter.create_bit_array()
         print('Number of items: {}'.format(new_filter.number_of_items))
         print('Bit array size: {}'.format(new_filter.number_of_bits))
         print('Number of hash functions: {}'.format(new_filter.number_of_hf))
-        print('Probability of false positives: {}'.format(new_filter.prop_false_positive))
+        print('Probability of false positives: {} \n'.format(new_filter.prop_false_positive))
     else:
         size_bit_array = input('Give the the size of the bit array:')
         num_of_hf = input('How many times you want to hash each item?')
+        print()
         new_filter = BloomFilter(None, int(num_items), int(size_bit_array), int(num_of_hf))
         print('Number of items: {}'.format(new_filter.number_of_items))
         print('Bit array size: {}'.format(new_filter.number_of_bits))
         print('Number of hash functions: {}'.format(new_filter.number_of_hf))
-        print('Probability of false positives: {}'.format(new_filter.prop_false_positive))
-            
-    # Initial call to print 0% progress
-    printProgressBar(0, len(dataset), prefix = 'Progress:', suffix = 'Complete', length = 50)
-    add_start = time.time()
-    for i, item in enumerate(dataset):
-        new_filter.add_item_to_filter(item, hash_functions[0])
-        # Update Progress Bar
-        printProgressBar(i + 1, len(dataset), prefix = 'Progress:', suffix = 'Complete', length = 50)
-    add_end = time.time()
-
-    print('Total time: {}s'.format(add_end - add_start))
-
-
-# print('1-- Give the size of the BF and the Probability of false positives(fraction between 0 and 1).')
-# print('2-- Give the size of the BF, the ')
-# dataset = importData('../Datasets/ips.txt')
-# l = len(dataset)
-
-# test_count = 0
-# test_dataset = importData('../Datasets/test_ips.txt')
-# for i in range(0,100000):
-#     test_dataset.append(dataset[i])
-# test_dataset1 = test_dataset
-
-# shuffle(test_dataset)
-# shuffle(test_dataset)
-# for size in range(1000000, 30000000, 1000000):
-#     bit_array = bitarray(int(size))
-#     bit_array.setall(0)
-#     for count in range(1,20):
-#         for func in hash_functions:
-#             print(str(size) + ", " + str(count) + ", " + str(func))
-#             # Initial call to print 0% progress
-#             printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
-#             curr_res = []
-#             true_positive = 0
-#             false_positive = 0
-#             # Add all the items to the bloom filter
-#             add_start = time.time()
-#             for i, item in enumerate(dataset):
-#                 add_item_to_filter(item, count, func, size)
-#                 # Update Progress Bar
-#                 printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
-#             add_end = time.time()
-#             # Check which items are inside 
-#             check_start = time.time()
-#             for i, item in enumerate(test_dataset):
-#                 if check_item(item, count, func, size) and item in test_dataset1:
-#                         true_positive = true_positive + 1
-#                 # Update Progress Bar
-#                 printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
-#             check_end = time.time()
-#             curr_res.append(str(size))
-#             curr_res.append(count)
-#             curr_res.append(func)
-#             curr_res.append(add_end - add_start)
-#             curr_res.append(check_end - check_start)
-#             curr_res.append(true_positive)
-#             curr_res.append(false_positive)
-#             results.append(curr_res)
-#             print(results)
-
-# results.append(test_count)
-# print(results)
-# exportData('results.txt', results)
+        print('Probability of false positives: {} \n'.format(new_filter.prop_false_positive))
+while True:
+    print('1-- Add item to the Bloom filter.')
+    print('2-- Check if an item exists')
+    print('3-- Exit')
+    add_or_check = None
+    while add_or_check is None:
+        add_or_check = input()
+        if add_or_check == '1':
+            add_or_check = True
+        elif add_or_check == '2':
+            add_or_check = False
+        elif add_or_check == '3':
+            exit(0)
+        else:
+            add_or_check = None
+            print('Please try again!')
+    print()
+    if add_or_check:
+        print('1-- Insert the items of the {} file into to the Bloom Filter.'.format(file_name))
+        print('2-- Add only one item.')
+        prompt = None
+        while prompt is None:
+            prompt = input()
+            if prompt == '1':
+                prompt = True
+            elif prompt == '2':
+                prompt = False
+            else:
+                prompt = None
+                print('Please try again!')
+        print()
+        if prompt:
+            # Initial call to print 0% progress
+            printProgressBar(0, len(dataset), prefix = 'Progress:', suffix = 'Complete', length = 50)
+            add_start = time.time()
+            for i, item in enumerate(dataset):
+                new_filter.add_item_to_filter(item, hash_functions[0])
+                # Update Progress Bar
+                printProgressBar(i + 1, len(dataset), prefix = 'Progress:', suffix = 'Complete', length = 50)
+            add_end = time.time()
+            print('Total time: {}s.'.format(round(add_end - add_start, 5)))
+        else:
+            input_item = input('Give the item that you want to insert: ')
+            add_start = time.time()
+            new_filter.add_item_to_filter(input_item, hash_functions[0])
+            add_end = time.time()
+            print('Total time: {}s.'.format(round(add_end - add_start, 5)))
+    else:
+        print('1-- Insert a file and check how many items are contained into the Bloom Filter')
+        print('2-- Check for one item')
+        prompt = None
+        while prompt is None:
+            prompt = input()
+            if prompt == '1':
+                prompt = True
+            elif prompt == '2':
+                prompt = False
+            else:
+                prompt = None
+                print('Please try again!')
+        print()
+        if prompt:
+            file_name = input('Please type in the path to your file and press "Enter":')
+            test_dataset = importData(file_name)
+            # Initial call to print 0% progress
+            printProgressBar(0, len(test_dataset), prefix = 'Progress:', suffix = 'Complete', length = 50)
+            counter = 0
+            check_start = time.time()
+            for i, item in enumerate(test_dataset):
+                if new_filter.check_item(item, hash_functions[0]):
+                    counter += 1
+                # Update Progress Bar
+                printProgressBar(i + 1, len(test_dataset), prefix = 'Progress:', suffix = 'Complete', length = 50)
+            check_end = time.time()
+            print('Total time: {}s'.format(check_end - check_start))
+            print('{} items were inside the Bloom Filter out of the {}.'.format(counter, len(test_dataset)))
+            print()
+        else:
+            input_item = input('Give the item that you want to check: ')
+            add_start = time.time()
+            if new_filter.check_item(input_item, hash_functions[0]):
+                print('The item was inside the Bloom Filter.')
+            else:
+                print('The item was not inside the Bloom Filter.')
+            add_end = time.time()
+            print('Total time: {}s'.format(round(add_end - add_start, 5)))
+            print()
